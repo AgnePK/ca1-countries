2527d5b45b914be0d4fc295ae7d4f2fbba395416
Agne Postnikova <N00211108@iadt.ie>
2023-10-26 12:21:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 
+//pages
+import Home from "./pages/Home.js";
+
+//components
+import Navbar from "./components/Navbar.js";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<h1>Hello from app</h1>
+			<Router>
+				<Container>
+					<Row>
+						<Col>
+							<Navbar />
+
+							<Routes>
+								<Route path="/" element={<Home />} />
+							</Routes>
+						</Col>
+					</Row>
+				</Container>
+			</Router>
+		</>
+	);
 }
 
 export default App;
---
+++ src/pages/View.js
@@ -0,0 +1,6 @@
+const View = () =>{
+    return(
+        <h1>View Page</h1>
+    )
+}
+export default View
